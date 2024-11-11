fe3f16215b3085b84380e62851c439f5ea6be78b
Codebolt <codebolt7@gmail.com>
2024-11-11 13:17:36 -0800

    Implemented Button for L16 Dialog

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,13 @@
-// to do
-alert("you clicked the button!");
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+const APP_NAME = "Geocoin Carrier";
+document.title = APP_NAME;
+app.innerHTML = APP_NAME;
+
+const alertButton = document.createElement("button");
+alertButton.textContent = "Alert Button";
+app.append(alertButton);
+
+alertButton.addEventListener("click", () => {
+  alert("you clicked the button!");
+});
